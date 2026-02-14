b63d1c0dc101f5e241766c227427c6e35b874aa1
joshtheposh8 <225066180+joshtheposh8@users.noreply.github.com>
2026-02-13 17:40:47 -0700

    final

--- src/ripple_adder.vhd
+++ src/ripple_adder.vhd
@@ -5,30 +5,29 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity rippleCarryAdder is
-    Port ( A : in STD_LOGIC_VECTOR (3 downto 0);
-           B : in STD_LOGIC_VECTOR (3 downto 0);
-           Cin : in STD_LOGIC;
-           S : out STD_LOGIC_VECTOR (3 downto 0);
-           Cout : out STD_LOGIC
-           );
-end rippleCarryAdder;
 
-architecture Behavioral of rippleCarryAdder is
+entity ripple_adder is
+    Port (
+        A    : in  STD_LOGIC_VECTOR (3 downto 0);
+        B    : in  STD_LOGIC_VECTOR (3 downto 0);
+        Cin  : in  STD_LOGIC;
+        S    : out STD_LOGIC_VECTOR (3 downto 0);
+        Cout : out STD_LOGIC
+    );
+end ripple_adder;
 
-    -- Declare components here
+architecture Behavioral of ripple_adder is
     component full_adder is
         port (
-            A     : in std_logic;
-            B     : in std_logic;
-            Cin   : in std_logic;
-            S     : out std_logic;
-            Cout  : out std_logic
-            );
-        end component full_adder;
-    -- Declare signals here
-    signal w_carry  : STD_LOGIC_VECTOR(3 downto 0); -- for ripple between adders
+            A    : in  std_logic;
+            B    : in  std_logic;
+            Cin  : in  std_logic;
+            S    : out std_logic;
+            Cout : out std_logic
+        );
+    end component;
 
+    signal w_carry : std_logic_vector(2 downto 0);
 begin
     full_adder_0: full_adder
     port map(
@@ -46,6 +45,22 @@ begin
         Cin   => w_carry(0),
         S     => S(1),
         Cout  => w_carry(1)
+    );
+        full_adder_2: full_adder
+    port map(
+        A     => A(2),
+        B     => B(2),
+        Cin   => w_carry(1),
+        S     => S(2),
+        Cout  => w_carry(2)
+    );
+        full_adder_3: full_adder
+    port map(
+        A     => A(3),
+        B     => B(3),
+        Cin   => w_carry(2),
+        S     => S(3),
+        Cout  => Cout
     );
 
 
